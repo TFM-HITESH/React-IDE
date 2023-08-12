083a346d344fc6cb9528ecef2237e5a196cdd332
TFM-HITESH <hiteshshivkumar@gmail.com>
2023-08-12 21:57:13 +0530

    Commit

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -246,7 +246,7 @@ const Landing = () => {
           "focus:outline-none w-[25%] border-2 border-black z-10 rounded-md shadow-[5px_5px_0px_0px_rgba(0,0,0)] px-4 py-2 hover:shadow transition duration-200 bg-gray-300 mt-2"
         )}>
           <OutputWindow outputDetails={outputDetails} />
-          <div className="flex flex-col items-end">
+          <div className="flex flex-col items-center">
             <CustomInput
               customInput={customInput}
               setCustomInput={setCustomInput}
